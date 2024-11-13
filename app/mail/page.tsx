f662caef0447e09a00953ff872f67d18619af94e
matiqueue <matiqueue@gmail.com>
2024-11-14 00:35:02 +0100

    del

--- app/mail/page.tsx
+++ app/mail/page.tsx
@@ -4,7 +4,7 @@ import { accounts, mails } from "@/app/mail/data"
 export default function MailPage() {
   return (
     <>
-      <div className="hidden flex-col md:flex">
+      <div className="p-3 hidden flex-col md:flex">
         <Mail accounts={accounts} mails={mails} navCollapsedSize={4} />
       </div>
     </>
