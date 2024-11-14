f8c33490ad4d91dd0ec91b18e7356f8a90377186
matiqueue <matiqueue@gmail.com>
2024-11-14 02:40:51 +0100

    log func

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function log(...args: string[]): void {
+  console.log(...args)
+}
